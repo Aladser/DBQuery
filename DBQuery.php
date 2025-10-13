30b3ff9c1601265950af7b4ff78b2d329f5d4fa3
Aladser <aladser@gmail.com>
2025-10-13 15:46:29 +0900

    Update DBQuery.php
    обновлены методы

--- DBQuery.php
+++ DBQuery.php
@@ -1,9 +1,5 @@
 <?php
 
-namespace App\Core;
-
-use PDO;
-
 /** Класс запросов в БД на основе PDO */
 class DBQuery
 {
@@ -12,24 +8,21 @@ class DBQuery
     private string $userDB;
     private string $passwordDB;
     private string $db_type;
-
-    private $isPermanentConnection;
     private $dbConnection;
     // массив поддерживаемых СУБД
     private static $DB_TYPE = ['mysql', 'pgsql', 'mssql', 'sqlite', 'sybase'];
 
     /**
-     * @param string $host                  хост
-     * @param string $nameDB                имя бд
-     * @param string $userDB                пользователь
-     * @param string $passwordDB            пароль
-     * @param string $db_type               тип БД: mysql, pgsql, mssql, sqlite, sybase
-     * @param bool   $isPermanentConnection флаг постоянного подключения
-     */
-    public function __construct(string $host, string $nameDB, string $userDB, string $passwordDB, string $db_type, bool $isPermanentConnection = false)
+     * @param string $host        хост
+     * @param string $nameDB      имя бд
+     * @param string $userDB      пользователь
+     * @param string $passwordDB  пароль
+     * @param string $db_type     тип БД: mysql, pgsql, mssql, sqlite, sybase
+    */
+    public function __construct(string $host, string $nameDB, string $userDB, string $passwordDB, string $db_type='mysql')
     {
         if (!in_array($db_type, DBQuery::$DB_TYPE)) {
-            throw new \Exception("указанный тип БД ($db_type) не поддерживается");
+            throw new \Exception("Указанный тип БД ($db_type) не поддерживается");
         }
 
         $this->host = $host;
@@ -37,159 +30,117 @@ class DBQuery
         $this->userDB = $userDB;
         $this->passwordDB = $passwordDB;
         $this->db_type = $db_type;
-        $this->isPermanentConnection = $isPermanentConnection;
-
-        if ($isPermanentConnection) {
-            $this->connect();
-        }
+        $this->dbConnection = new \PDO("$this->db_type:dbname=$nameDB; host=$host", $userDB, $passwordDB);
     }
-
-    /** подключение к БД */
-    private function connect()
-    {
-        try {
-            $this->dbConnection = new \PDO(
-                "$this->db_type:dbname=$this->nameDB; host=$this->host",
-                $this->userDB,
-                $this->passwordDB
-            );
-        } catch (\PDOException $e) {
-            exit($e->getMessage());
-        }
-    }
-
-    /** отключение от БД */
-    private function disconnect()
-    {
-        $this->dbConnection = null;
-    }
-
-    /** insert операции */
-    public function insert(string $sql, array $args): int
-    {
-        if (!$this->isPermanentConnection) {
-            $this->connect();
-        }
-
-        $stmt = $this->dbConnection->prepare($sql);
-        $stmt->execute($args);
-        $id = $this->dbConnection->lastInsertId();
-
-        if (!$this->isPermanentConnection) {
-            $this->disconnect();
-        }
-
-        return $id;
-    }
-
-    /** update операции */
-    public function update(string $sql, array $args): bool
-    {
-        if (!$this->isPermanentConnection) {
-            $this->connect();
-        }
-
-        $stmt = $this->dbConnection->prepare($sql);
-        $stmt->execute($args);
-        $rowCount = $stmt->rowCount();
-
-        if (!$this->isPermanentConnection) {
-            $this->disconnect();
-        }
-
-        return $rowCount > 0;
-    }
-
-    /** delete операции */
-    public function delete(string $sql, array $args): bool
-    {
-        return $this->update($sql, $args);
-    }
-
-    /** выполняет подготовленный запрос
-     * @param bool $isOneValue одно или множество запрашиваемых полей
-     *
+    
+    /** Выполняет подготовленный запрос
+     * 
+     * @param bool $is_one_value одно или множество запрашиваемых полей
      * @return mixed массив строк или одно значение
-     */
-    public function queryPrepared(string $sqlExpession, ?array $arguments = null, bool $isOneValue = true)
+    */
+    public function queryPrepared(string $sql, array $arguments=[], bool $is_one_value=true)
     {
-        if (!$this->isPermanentConnection) {
-            $this->connect();
-        }
-
-        $stmt = $this->dbConnection->prepare($sqlExpession);
-        if (!empty($arguments)) {
+        $stmt = $this->dbConnection->prepare($sql);
+        if (count($arguments)>0) {
             $stmt->execute($arguments);
         } else {
             $stmt->execute();
         }
-
-        if (!$this->isPermanentConnection) {
-            $this->disconnect();
-        }
-
-        return $isOneValue ? $stmt->fetch(\PDO::FETCH_ASSOC) : $stmt->fetchAll();
+        return $is_one_value ? $stmt->fetch(\PDO::FETCH_ASSOC) : $stmt->fetchAll();
     }
 
-    /** выполняет запрос
-     * @param string $sql        запрос
-     * @param bool   $isOneValue одно или множество запрашиваемых полей
+    /** Выполняет запрос
+     * @param string $sql          запрос
+     * @param bool   $is_one_value одно или множество запрашиваемых полей
      *
      * @return mixed массив строк или одно значение
-     */
-    public function query(string $sql, bool $isOneValue = true)
+    */
+    public function query(string $sql, bool $is_one_value = true)
     {
-        if (!$this->isPermanentConnection) {
-            $this->connect();
-        }
-
-        $query = $this->dbConnection->query($sql);
-
-        if (!$this->isPermanentConnection) {
-            $this->disconnect();
-        }
-
-        return $isOneValue ? $query->fetch(\PDO::FETCH_ASSOC) : $query->fetchAll();
+        $data = $this->dbConnection->query($sql);
+        return $is_one_value ? $data->fetch(\PDO::FETCH_ASSOC) : $data->fetchAll();
     }
+    
+    /**
+     * INSERT
+     * @param $table_name имя таблицы
+     * @param $fields_array массив полей строки
+     * @return id
+    */
+    public function insert(string $table_name, array $fields_array): int
+    {
+        if (count($fields_array)===0) {
+            return 0;
+        }
 
-    /** выполняет изменения данных.
+        $names_str = implode(', ', array_keys($fields_array));
+        $name_placeholders_str = ':'.implode(', :', array_keys($fields_array));
+        $stmt = $this->dbConnection->prepare("INSERT INTO {$table_name}($names_str) VALUES($name_placeholders_str)");
+        $stmt->execute($fields_array);
+
+        return $this->dbConnection->lastInsertId();
+    }
+    
+    /**
+     * UPDATE
+     * @param string $table_name имя таблицы
+     * @param array $fields_array массив полей строки
+     * @param integer $id id строки
+     * @return integer
+    */
+    public function update(string $table_name, array $fields_array, int $id): int
+    {
+        if (count($fields_array)===0) {
+            return false;
+        }
+
+        $values_string = '';
+        foreach (array_keys($fields_array) as $key) {
+            $values_string .= "$key = :$key, ";
+        }
+        $values_string = mb_substr($values_string, 0, mb_strlen($values_string)-2);
+        $fields_array['id'] = $id;
+
+        $stmt = $this->dbConnection->prepare("UPDATE $table_name SET $values_string WHERE id=:id");
+        $stmt->execute($fields_array);
+        
+        return $stmt->rowCount();
+    }
+    
+    /**
+     * DELETE
+     * @param $table_name имя таблицы
+     * @param $where_condition условие
+     * @param $args массив параметров WHERE условия
+     * @return integer
+    */
+    public function delete(string $table_name, int $id): int
+    {
+        $stmt = $this->dbConnection->prepare("DELETE FROM $table_name WHERE id=:id");
+        $stmt->execute(['id'=>$id]);
+        return $stmt->rowCount();
+    }
+    
+    /** Выполняет изменения данных.
      * @param string $sql запрос
      *
      * @return false|int число измененных строк
-     */
+    */
     public function exec(string $sql)
     {
-        if (!$this->isPermanentConnection) {
-            $this->connect();
-        }
-
-        $numRows = $this->dbConnection->exec($sql);
-
-        if (!$this->isPermanentConnection) {
-            $this->disconnect();
-        }
-
-        return $numRows;
+        return $this->dbConnection->exec($sql);
     }
 
-    /** выполняет процедуру с возвращаемым результатом
+    /** Выполняет процедуру с возвращаемым результатом
      * @param mixed $sql выражение
      * @param mixed $out выходная переменная, куда будет возвращен результат
-     */
+    */
     public function executeProcedure($sql, $out)
     {
-        if (!$this->isPermanentConnection) {
-            $this->connect();
-        }
-
         $stmt = $this->dbConnection->prepare("call $sql");
         $stmt->execute();
         $stmt->closeCursor();
         $procRst = $this->dbConnection->query("select $out as info");
-
-        if (!$this->isPermanentConnection) {
-            $this->disconnect();
-        }
 
         return $procRst->fetch(\PDO::FETCH_ASSOC)['info'];
     }
